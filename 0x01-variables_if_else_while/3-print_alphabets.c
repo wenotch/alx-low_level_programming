31048096df9133ba486fdc620b2686f384ce713c
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-10-14 06:07:48 +0100

    solution

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A program that prints both alphabet and lowercase
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-this is it
+This is a readme file for 0x01. C - Variables, if, else, while, ALX fullstack development
